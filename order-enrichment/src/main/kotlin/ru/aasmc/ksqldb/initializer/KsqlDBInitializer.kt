c74e1804fbc70b2e9037c1d999986c43182d1d38
aleksas <aasmc@yandex.ru>
2023-11-15 13:32:57 +0300

    fix: bugs with avro schema due to kafka streams cached statedir

--- order-enrichment/src/main/kotlin/ru/aasmc/ksqldb/initializer/KsqlDBInitializer.kt
+++ order-enrichment/src/main/kotlin/ru/aasmc/ksqldb/initializer/KsqlDBInitializer.kt
@@ -18,87 +18,75 @@ class KsqlDBInitializer(
     ksqlDBProps: KsqlDBProps,
 ) : ApplicationListener<ContextRefreshedEvent> {
 
-    private val CREATE_ORDERS_STREAM = """
-        CREATE STREAM IF NOT EXISTS ${ksqlDBProps.ordersStream} (
-            ID STRING KEY, 
-            CUSTOMERID BIGINT, 
-            STATE STRING, 
-            PRODUCT STRING, 
-            QUANTITY INT, 
-            PRICE DOUBLE 
-        ) 
-        WITH (
-            KAFKA_TOPIC='${topicProps.ordersTopic}',
-            VALUE_FORMAT='AVRO';
-        );
-    """
+    private val CREATE_ORDERS_STREAM = "CREATE SOURCE STREAM IF NOT EXISTS ${ksqlDBProps.ordersStream} (" +
+                "ID STRING KEY, " +
+                "CUSTOMERID BIGINT, " +
+                "STATE STRING, " +
+                "PRODUCT STRING, " +
+                "QUANTITY INT, " +
+                "PRICE DOUBLE) WITH (" +
+                "kafka_topic='${topicProps.ordersTopic}'," +
+                " value_format='AVRO'" +
+            ");"
 
     private val CREATE_CUSTOMERS_TABLE = """
           CREATE SOURCE TABLE IF NOT EXISTS ${ksqlDBProps.customersTable} (
-              CUSTOMERID BIGINT PRIMARY KEY,
-              FIRSTNAME STRING,
-              LASTNAME STRING,
-              EMAIL STRING,
-              ADDRESS STRING,
-              LEVEL STRING DEFAULT 'bronze'
-          )
-          WITH (
+              CUSTOMERID BIGINT PRIMARY KEY, 
+              FIRSTNAME STRING, 
+              LASTNAME STRING, 
+              EMAIL STRING, 
+              ADDRESS STRING, 
+              LEVEL STRING
+          ) WITH (
               KAFKA_TOPIC='${topicProps.customersTopic}',
-              VALUE_FORMAT='AVRO',
+              VALUE_FORMAT='AVRO'
           );
     """
 
     private val CREATE_ORDERS_ENRICHED_STREAM = """
-        CREATE STREAM IF NOT EXISTS ${ksqlDBProps.ordersEnrichedStream}
-            AS SELECT ${ksqlDBProps.customersTable}.CUSTOMERID AS customerId,
-                      ${ksqlDBProps.customersTable}.FIRSTNAME,
-                      ${ksqlDBProps.customersTable}.LASTNAME,
-                      ${ksqlDBProps.customersTable}.LEVEL,
-                      ${ksqlDBProps.ordersStream}.PRODUCT,
-                      ${ksqlDBProps.ordersStream}.QUANTITY,
-                      ${ksqlDBProps.ordersStream}.PRICE
-            FROM ${ksqlDBProps.ordersStream}
-            LEFT JOIN ${ksqlDBProps.customersTable}
+        CREATE STREAM IF NOT EXISTS ${ksqlDBProps.ordersEnrichedStream} 
+            AS SELECT ${ksqlDBProps.customersTable}.CUSTOMERID AS customerId, 
+                      ${ksqlDBProps.customersTable}.FIRSTNAME, 
+                      ${ksqlDBProps.customersTable}.LASTNAME, 
+                      ${ksqlDBProps.customersTable}.LEVEL, 
+                      ${ksqlDBProps.ordersStream}.PRODUCT, 
+                      ${ksqlDBProps.ordersStream}.QUANTITY, 
+                      ${ksqlDBProps.ordersStream}.PRICE 
+            FROM ${ksqlDBProps.ordersStream} 
+            LEFT JOIN ${ksqlDBProps.customersTable} 
                  ON ${ksqlDBProps.ordersStream}.CUSTOMERID = ${ksqlDBProps.customersTable}.CUSTOMERID;
        """
 
     private val CREATE_FRAUD_ORDER_TABLE = """
         CREATE TABLE IF NOT EXISTS ${ksqlDBProps.fraudOrderTable} 
-        WITH (KEY_FORMAT='json') 
-        AS SELECT CUSTOMERID,
-                  LASTNAME,
-                  FIRSTNAME,
-                  COUNT(*) AS COUNTS
-           FROM ${ksqlDBProps.ordersEnrichedStream} 
-           WINDOW TUMBLING (SIZE 30 SECONDS)
-           GROUP BY CUSTOMERID, LASTNAME, FIRSTNAME
-           HAVING COUNT(*)>2;
+        WITH (KEY_FORMAT='json')  
+        AS SELECT CUSTOMERID, 
+                  LASTNAME, 
+                  FIRSTNAME, 
+                  COUNT(*) AS COUNTS 
+           FROM ${ksqlDBProps.ordersEnrichedStream}  
+           WINDOW TUMBLING (SIZE 30 SECONDS) 
+           GROUP BY CUSTOMERID, LASTNAME, FIRSTNAME 
+           HAVING COUNT(*)>2; 
     """
 
     override fun onApplicationEvent(event: ContextRefreshedEvent) {
         try {
             val ordersResult = client.executeStatement(CREATE_ORDERS_STREAM).get()
             log.info(
-                "Result of creating orders stream: {}", ordersResult.queryId()
-                    .orElse("Failed to create orders stream.")
+                "Result of creating orders stream: {}", ordersResult
             )
-            Thread.sleep(1000)
             val customersResult = client.executeStatement(CREATE_CUSTOMERS_TABLE).get()
             log.info(
-                "Result of creating customers table: {}", customersResult.queryId()
-                    .orElse("Failed to create customers table.")
+                "Result of creating customers table: {}", customersResult
             )
-            Thread.sleep(1000)
             val enrichedResult = client.executeStatement(CREATE_ORDERS_ENRICHED_STREAM).get()
             log.info(
-                "Result of creating orders enriched stream: {}", enrichedResult.queryId()
-                    .orElse("Failed to create orders_enriched_stream")
+                "Result of creating orders enriched stream: {}", enrichedResult
             )
-            Thread.sleep(1000)
             val fraudResult = client.executeStatement(CREATE_FRAUD_ORDER_TABLE).get()
             log.info(
-                "Result of creating fraud order table: {}", fraudResult.queryId()
-                    .orElse("Failed to create fraud order table.")
+                "Result of creating fraud order table: {}", fraudResult
             )
         } catch (e: Exception) {
             log.error("Exception while initializing ksqlDB. Message: {}", e.message)
