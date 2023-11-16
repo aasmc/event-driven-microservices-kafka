54f5a2307e89d76534ff382e637b8779dcfd7c38
Alex Alexeyev <aasmc@yandex.ru>
2023-11-16 19:05:29 +0300

    update: prepare scripts

--- prepare_script_docker_new.sh
+++ prepare_script_docker_new.sh
@@ -12,9 +12,14 @@ check_connect_up() {
 
 WARMUP_TIME=180
 printf "\n====== Starting infrastructure services in Docker\n"
-docker compose up -d --build zookeeper kafka schema-registry ksqldb connect sqlite elasticsearch
+docker compose up -d --build
 printf "\n====== Giving infrastructure services $WARMUP_TIME seconds to startup\n"
 sleep $WARMUP_TIME
+
+MAX_WAIT=200
+echo "Waiting up to $MAX_WAIT seconds for connect to start"
+retry $MAX_WAIT check_connect_up connect || exit 1
+printf "\n\n"
 
 printf "\n====== Registering Avro schema with Schema Registry\n"
 ./gradlew registerSchemaTask
@@ -24,17 +29,6 @@ printf "\n====== Building Microservices\n"
 printf "\n====== Giving microservices $WARMUP_TIME seconds to build\n"
 sleep $WARMUP_TIME
 
-printf "\n====== Starting microservices in Docker\n"
-docker compose up -d email-service fraud-service order-details-service order-enrichment-service orders-service validation-aggregator-service
-printf "\n====== Giving microservices $WARMUP_TIME seconds to startup\n"
-sleep $WARMUP_TIME
-
-MAX_WAIT=100
-echo "Waiting up to $MAX_WAIT seconds for connect to start"
-retry $MAX_WAIT check_connect_up connect || exit 1
-printf "\n\n"
-
-
 printf "\n====== Configuring Elasticsearch mappings\n"
 ./infra/dashboard/set_elasticsearch_mapping.sh
 
@@ -43,6 +37,3 @@ printf "\n====== Submitting connectors\n\n"
 
 printf "\n\n====== Configuring Kibana Dashboard\n"
 ./infra/dashboard/configure_kibana_dashboard.sh
-
-printf "\n\n====== Reading data from topics and ksqlDB\n"
-./read-topics-ccloud.sh
---
+++ prepare_script_docker_old.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+check_connect_up() {
+  containerName=$1
+
+  FOUND=$(docker compose logs $containerName | grep "Herder started")
+  if [ -z "$FOUND" ]; then
+    return 1
+  fi
+  return 0
+}
+
+WARMUP_TIME=180
+printf "\n====== Starting infrastructure services in Docker\n"
+docker-compose up -d --build
+printf "\n====== Giving infrastructure services $WARMUP_TIME seconds to startup\n"
+sleep $WARMUP_TIME
+
+MAX_WAIT=200
+echo "Waiting up to $MAX_WAIT seconds for connect to start"
+retry $MAX_WAIT check_connect_up connect || exit 1
+printf "\n\n"
+
+printf "\n====== Registering Avro schema with Schema Registry\n"
+./gradlew registerSchemaTask
+
+printf "\n====== Building Microservices\n"
+./gradlew clean build
+printf "\n====== Giving microservices $WARMUP_TIME seconds to build\n"
+sleep $WARMUP_TIME
+
+printf "\n====== Configuring Elasticsearch mappings\n"
+./infra/dashboard/set_elasticsearch_mapping.sh
+
+printf "\n====== Submitting connectors\n\n"
+./scripts/create_connectors.sh
+
+printf "\n\n====== Configuring Kibana Dashboard\n"
+./infra/dashboard/configure_kibana_dashboard.sh
